7c9eabde3e09736f666637f4d568d6ce7256a5cc
UHquokka <bananamayong@gmail.com>
2023-10-06 14:52:08 +0900

    feat: coming-soon , now-playing page 생성

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { getComingSoon } from "../api";
+import Movies from "../components/Moives/Movies";
 
 export default function ComingSoon() {
+    const { isLoading, data } = useQuery({
+        queryKey: ['coming-soon'],
+        queryFn: getComingSoon
+    });
     return (
         <div>
-            
+            {isLoading ? <span>Loading...</span>
+                : <Movies data={data} />
+            }
         </div>
     );
 }
-
--- src/pages/NowPlaying.tsx
+++ src/pages/NowPlaying.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getNowPlaying } from '../api';
+import Movies from '../components/Moives/Movies';
 
 export default function NowPlaying() {
+    const { isLoading, data } = useQuery({
+        queryKey: ['now-playing'],
+        queryFn: getNowPlaying
+    });
     return (
         <div>
-            <h1>  Now Playing</h1>
+            {isLoading ? <span>Loading...</span>
+                : <Movies data={data} />
+            }
         </div>
     );
 }
