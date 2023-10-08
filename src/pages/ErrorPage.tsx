b0c910969b5b9e559d85120c281651abc9474d0e
UHquokka <bananamayong@gmail.com>
2023-10-09 02:09:35 +0900

    chore: 에러 페이지 수정

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function ErrorPage() {
     return (
-        <div>
-            Sorry. wrong url
-
-        </div>
+        <WrongPage>
+            <Image src='src/assets/imgs/errorDdozza.png' />
+            <span>Wrong Page..!</span>
+            <Link to='/'>👉Back to Home</Link>
+        </WrongPage>
     );
 }
+const WrongPage = styled.section`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+const Image = styled.img`
+    width: 60%;
+    height: 60%;
+`
 
